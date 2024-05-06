c61fa5dc21c95afd2fcd64a54c31302a2824d833
JuliaMelle <54196660+JuliaMelle@users.noreply.github.com>
2024-05-06 22:51:36 +0800

    route fix and frontend of recipe and navbar fix

--- src/App.js
+++ src/App.js
@@ -1,159 +1,44 @@
-// App.js
-
-import React, { useEffect, useState } from "react";
-import RecipeCard from "./RecipeAPI/RecipeCard";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-<<<<<<< Updated upstream
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useParams,
+} from "react-router-dom";
+import Navbar from "./Navbar";
+import Signup from "./Signup";
+import RecipeMain from "./RecipeAPI/RecipeMain"; // Assuming this is the correct component
 import RecipeDetails from "./RecipeAPI/RecipeDetails";
 
-=======
-import RecipeDetails from "./RecipeDetails";
-import Navbar from "./Navbar"; // Import the Navbar component
-import Signup from "./Signup";
->>>>>>> Stashed changes
 const App = () => {
-  const APP_ID = "b5e8ffaa";
-  const APP_KEY = "366c56854db11413d53540a02d074f37";
-  const [food_recipes, setfood_recipes] = useState([]);
-  const [search_recipe, setSearch_recipe] = useState("");
-  const [search_query, setSearch_Query] = useState("chicken");
-
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    getRecipesFunction();
-  }, [search_query]);
-
-  const getRecipesFunction = async () => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        `https://api.edamam.com/search?q=${search_query}&app_id=${APP_ID}&app_key=${APP_KEY}`
-      );
-      if (!response.ok) {
-        throw new Error(`API request failed with status ${response.status}`);
-      }
-      const data = await response.json();
-      // Check if data.hits is an array before setting the state
-      if (Array.isArray(data.hits)) {
-        setfood_recipes(data.hits);
-        console.log(data);
-      } else {
-        // Handle the case where data.hits is not an array
-        setError("Unexpected response structure");
-      }
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  const updateSearchFunction = (e) => {
-    setSearch_recipe(e.target.value);
-  };
-
-  const getSearchFunction = (e) => {
-    e.preventDefault();
-    setSearch_Query(search_recipe);
-    setSearch_recipe("");
-  };
   const [isDialogOpen, setIsDialogOpen] = useState(false);
 
-  const openDialog = () => {
-    setIsDialogOpen(true);
-  };
-
-  // Function to close the dialog
-  const closeDialog = () => {
-    setIsDialogOpen(false);
-  };
   return (
     <div className="bg-blue-50 min-h-screen font-sans">
-      {/* <header className="bg-blue-500 py-4 text-white">
-        <div className="container mx-auto text-center">
-          <h1
-            className="text-3xl sm:text-4xl  
-                                   md:text-5xl lg:text-6xl 
-                                   font-extrabold tracking-tight"
-          >
-            <span className="block">RECIPES</span>
-          </h1>
-        </div>
-      </header> */}
-      <Navbar /> {/* Include the Navbar here */}
-      <div
-        className="container mx-auto mt-8 p-4  
-                            sm:px-6 lg:px-8"
-      >
-        <form
-          onSubmit={getSearchFunction}
-          className="bg-white p-4 sm:p-6  
-                               lg:p-8 rounded-lg shadow-md  
-                               flex flex-col sm:flex-row items-center  
-                               justify-center space-y-4 sm:space-y-0  
-                               sm:space-x-4"
-        >
-          <div
-            className="relative justify-center flex-grow 
-                                    w-full sm:w-1/2"
-          >
-            <input
-              type="text"
-              name="search"
-              value={search_recipe}
-              onChange={updateSearchFunction}
-              placeholder="Search for recipes..."
-              className="w-full py-3 px-4 bg-gray-100  
-                                       border border-blue-300 focus:ring-blue-500  
-                                       focus:border-blue-500 rounded-full  
-                                       text-gray-700 outline-none transition-colors  
-                                       duration-200 ease-in-out focus:ring-2  
-                                       focus:ring-blue-900 focus:bg-transparent  
-                                       focus:shadow-md"
-            />
-          </div>
-          <button
-            type="submit"
-            className="bg-blue-500 hover:bg-blue-600 focus:ring-2  
-                        focus:ring-blue-900 text-white font-semibold py-3 px-6  
-                        rounded-full transform hover:scale-105 transition-transform  
-                        focus:outline-none focus:ring-offset-2  
-                        focus:ring-offset-blue-700"
-          >
-            Search Recipe
-          </button>
-        </form>
-      </div>
-      <div className="container mx-auto mt-8 p-4 sm:px-6 lg:px-8">
-        <Routes>
-          <Route
-            path="/recipe/:id"
-            element={
-              <RecipeDetails open={isDialogOpen} setOpen={setIsDialogOpen} />
-            }
-          />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-
-        <div
-          className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  
-                lg:grid-cols-4 gap-4"
-        >
-          {food_recipes.map((recipe) => (
-            <RecipeCard
-              key={recipe.recipe.label}
-              recipe={recipe.recipe}
-              setIsDialogOpen={setIsDialogOpen}
-            />
-          ))}
-        </div>
-      </div>
-      {isLoading && <p>Loading recipes...</p>}
-      {error && <p>Error: {error}</p>}{" "}
+      <Navbar />
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/"
+          element={<RecipeMain open={isDialogOpen} setOpen={setIsDialogOpen} />}
+        />
+        <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
+      </Routes>
     </div>
+  );
+};
+
+// Wrapper component to pass props to RecipeDetails
+const RecipeDetailsWrapper = () => {
+  // const { id } = useParams(); // Access the id parameter from the route
+  const [isDialogOpen, setIsDialogOpen] = useState(true); // State for dialog
+
+  return (
+    <>
+      {" "}
+      <RecipeMain open={isDialogOpen} setOpen={setIsDialogOpen} />
+      <RecipeDetails open={isDialogOpen} setOpen={setIsDialogOpen} />
+    </>
   );
 };
 
--- src/RecipeAPI/RecipeDetails.js
+++ src/RecipeAPI/RecipeDetails.js
@@ -27,6 +27,7 @@ const RecipeDetails = ({ open, setOpen }) => {
         const data = await response.json();
         // Assuming the API returns the recipe details directly, adjust as necessary
         setRecipe(data.hits[0]); // Assuming the first hit is the desired recipe
+        console.log("Open state:", open);
       } catch (error) {
         console.error("Error fetching recipe details:", error);
       }
@@ -50,80 +51,90 @@ const RecipeDetails = ({ open, setOpen }) => {
     //     ))}
     //   </ul>
     // </div>
-    <Transition.Root show={open} as={Fragment}>
-      <Dialog
-        className="relative z-10"
-        initialFocus={cancelButtonRef}
-        onClose={setOpen}
-      >
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
+    <>
+      <Transition.Root show={open} as={Fragment} unmount={false}>
+        <Dialog
+          className="relative z-10"
+          initialFocus={cancelButtonRef}
+          onClose={setOpen}
         >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-        </Transition.Child>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
 
-        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              enterTo="opacity-100 translate-y-0 sm:scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                  <div className="sm:flex sm:items-start">
-                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                      <Dialog.Title
-                        as="h3"
-                        className="text-base font-semibold leading-6 text-gray-900"
-                      >
-                        <p>Ingredients:</p>
-                      </Dialog.Title>
-                      <div className="mt-2">
-                        <p className="text-sm text-gray-500">
-                          <h1>{recipe.recipe.label}</h1>
-                          <img
-                            src={recipe.recipe.image}
-                            alt={recipe.recipe.label}
-                          />
-                          <ul>
-                            {recipe.recipe.ingredientLines.map(
-                              (ingredient, index) => (
-                                <li key={index}>{ingredient}</li>
-                              )
-                            )}
-                          </ul>
-                        </p>
+          <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                  <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                    <div className="sm:flex sm:items-start">
+                      <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                        <Dialog.Title
+                          as="h3"
+                          className="text-base font-semibold leading-6 text-gray-900"
+                        >
+                          <p>Ingredients:</p>
+                        </Dialog.Title>
+                        <div className="mt-2">
+                          <p className="text-sm text-gray-500">
+                            <h1 className="text-lg font-bold mb-2">
+                              {recipe.recipe.label}
+                            </h1>
+                            <div className="flex justify-center items-center">
+                              <img
+                                src={recipe.recipe.image}
+                                alt={recipe.recipe.label}
+                                className="w-full h-auto mb-4 rounded-lg shadow-md"
+                              />
+                            </div>
+
+                            <ul className="list-disc list-inside">
+                              {recipe.recipe.ingredientLines.map(
+                                (ingredient, index) => (
+                                  <li key={index} className="mb-1">
+                                    {ingredient}
+                                  </li>
+                                )
+                              )}
+                            </ul>
+                          </p>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                  <button
-                    type="button"
-                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                    onClick={() => setOpen(false)}
-                    ref={cancelButtonRef}
-                  >
-                    Close
-                  </button>
-                </div>
-              </Dialog.Panel>
-            </Transition.Child>
+                  <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                    <button
+                      type="button"
+                      className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                      onClick={() => setOpen(false)}
+                      ref={cancelButtonRef}
+                    >
+                      Close
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
           </div>
-        </div>
-      </Dialog>
-    </Transition.Root>
+        </Dialog>
+      </Transition.Root>
+    </>
   );
 };
 
