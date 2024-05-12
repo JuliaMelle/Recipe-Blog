5939ad3145cb2489cb7841f4be913f9a03e2e052
Ashley Manaog <manaogashley06@gmail.com>
2024-05-12 20:06:06 +0800

    working signup using firebase

---
+++ src/FirebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCCWAWRZcbdRuXO_q5xiBxWGSi-TGleZZ8",
+  authDomain: "gourmetgatherings-e47b3.firebaseapp.com",
+  projectId: "gourmetgatherings-e47b3",
+  storageBucket: "gourmetgatherings-e47b3.appspot.com",
+  messagingSenderId: "712680618459",
+  appId: "1:712680618459:web:c82b9a014e7419771c4ffe",
+};
+
+const app = initializeApp(firebaseConfig);
+export const database = getAuth(app);
