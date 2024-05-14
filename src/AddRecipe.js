172617c048db677d9f1a7950b5730a5e0e9425a2
Ashley Manaog <manaogashley06@gmail.com>
2024-05-15 02:57:05 +0800

    improved frontend of add recipe

--- src/AddRecipe.js
+++ src/AddRecipe.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import app from "./FirebaseConfig";
 import { getDatabase, ref, set, push } from "firebase/database";
+import backgroundImage from "./assets/bg.jpg"; // adjust the path to your image
 
 function AddRecipe() {
   const [inputLabel, setInputLabel] = useState("");
@@ -11,7 +12,6 @@ function AddRecipe() {
   const [tempRecipeData, setTempRecipeData] = useState([]);
 
   const saveData = async () => {
-    //console.log(inputDishType);
     const db = getDatabase(app);
     const newDocRef = push(ref(db, "communityRecipes"));
     set(newDocRef, {
@@ -48,91 +48,111 @@ function AddRecipe() {
   };
 
   return (
-    <div className="p-6">
-      <form className="space-y-4">
-        <div className="flex flex-col">
-          <label
-            htmlFor="inputLbl"
-            className="mb-2 block text-sm font-medium text-gray-700"
-          >
-            Dish Name:
-          </label>
-          <input
-            type="text"
-            id="inputLbl"
-            value={inputLabel}
-            onChange={(e) => setInputLabel(e.target.value)}
-            className="border border-gray-300 rounded-md p-2"
-          />
-        </div>
-
-        <div className="flex flex-col">
-          <label
-            htmlFor="inputDT"
-            className="mb-2 block text-sm font-medium text-gray-700"
-          >
-            Dish Type:
-          </label>
-          <input
-            type="text"
-            id="inputDT"
-            value={inputDishType}
-            onChange={(e) => setInputDishType(e.target.value)}
-            className="border border-gray-300 rounded-md p-2"
-          />
-        </div>
-
-        <div className="flex flex-col">
-          <label
-            htmlFor="inputImg"
-            className="mb-2 block text-sm font-medium text-gray-700"
-          >
-            Dish Image:
-          </label>
-          <input
-            type="text"
-            id="inputImg"
-            value={inputImage}
-            onChange={(e) => setInputImage(e.target.value)}
-            className="border border-gray-300 rounded-md p-2"
-          />
-        </div>
-
-        {fields.map((field, index) => (
-          <div key={index} className="flex flex-col space-y-2">
+    <div
+      className="relative p-6 flex justify-center items-center min-h-screen bg-gray-100"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div className="relative bg-white shadow-lg rounded-lg p-8 w-full max-w-md ">
+        <h2 className="text-xl font-bold mb-4 text-center">Add a New Recipe</h2>
+        <form className="space-y-4">
+          <div className="flex flex-col">
+            <label
+              htmlFor="inputLbl"
+              className="mb-2 block text-sm font-medium text-gray-700"
+            >
+              Dish Name:
+            </label>
             <input
               type="text"
-              name="ingredient"
-              value={field.ingredient}
-              onChange={(event) => handleInputChange(index, event)}
-              placeholder="Ingredient"
-              className="border border-gray-300 rounded-md p-2"
-            />
-            <input
-              type="text"
-              name="quantity"
-              value={field.quantity}
-              onChange={(event) => handleInputChange(index, event)}
-              placeholder="Quantity"
+              id="inputLbl"
+              value={inputLabel}
+              onChange={(e) => setInputLabel(e.target.value)}
               className="border border-gray-300 rounded-md p-2"
             />
           </div>
-        ))}
 
-        <button
-          type="button"
-          onClick={() => handleAddFields()}
-          className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
-        >
-          Add More Ingredients
-        </button>
-        <button
-          onClick={handleSubmit}
-          className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600"
-        >
-          Save Recipe
-        </button>
-      </form>
+          <div className="flex flex-col">
+            <label
+              htmlFor="inputDT"
+              className="mb-2 block text-sm font-medium text-gray-700"
+            >
+              Dish Type:
+            </label>
+            <input
+              type="text"
+              id="inputDT"
+              value={inputDishType}
+              onChange={(e) => setInputDishType(e.target.value)}
+              className="border border-gray-300 rounded-md p-2"
+            />
+          </div>
+
+          <div className="flex flex-col">
+            <label
+              htmlFor="inputImg"
+              className="mb-2 block text-sm font-medium text-gray-700"
+            >
+              Dish Image Link:
+            </label>
+            <input
+              type="text"
+              id="inputImg"
+              value={inputImage}
+              onChange={(e) => setInputImage(e.target.value)}
+              className="border border-gray-300 rounded-md p-2"
+            />
+          </div>
+
+          {fields.map((field, index) => (
+            <div key={index} className="flex flex-col space-y-2">
+              <label
+                htmlFor="inputImg"
+                className="mb-2 block text-sm font-medium text-gray-700"
+              >
+                Ingredient:
+              </label>
+
+              <input
+                type="text"
+                name="ingredient"
+                value={field.ingredient}
+                onChange={(event) => handleInputChange(index, event)}
+                placeholder="Ingredient"
+                className="border border-gray-300 rounded-md p-2"
+              />
+              <input
+                type="text"
+                name="quantity"
+                value={field.quantity}
+                onChange={(event) => handleInputChange(index, event)}
+                placeholder="Quantity"
+                className="border border-gray-300 rounded-md p-2"
+              />
+            </div>
+          ))}
+
+          <div className="flex justify-center space-x-4">
+            <button
+              type="button"
+              onClick={handleAddFields}
+              className="bg-custom-coffee text-white px-4 py-2 rounded-md hover:bg-custom-brown"
+            >
+              Add More Ingredients
+            </button>
+            <button
+              onClick={handleSubmit}
+              className="bg-custom-purple text-white px-4 py-2 rounded-md hover:bg-custom-brown"
+            >
+              Save Recipe
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
